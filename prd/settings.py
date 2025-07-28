adab60dfff83a3953aec5f7e7da2dffcc042a5c7
Esak <esakmulushewa@gmail.com>
2025-07-28 12:38:15 +0300

    display modified

--- prd/settings.py
+++ prd/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-+(z54=9zcde++%88x!(j_tzb7h$4hpw4l9ju=d%%gna-ms8szj'
 import os
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-+(z54=9zcde++%88x!(j_tzb7h$4hpw4l9ju=d%%gna-ms8szj')# SECURITY WARNING: don't run with debug turned on in production!
