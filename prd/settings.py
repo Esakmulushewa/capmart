00b0c33f0f20091d2d41e73d3d741e1c454e2842
Esakmulushewa <123089710+Esakmulushewa@users.noreply.github.com>
2025-10-26 01:17:10 +0300

    Update settings.py

--- prd/settings.py
+++ prd/settings.py
@@ -121,7 +121,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, '/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_URL= 'https://captainmart.et/media/'
+MEDIA_URL= '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
